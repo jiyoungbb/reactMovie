67106f58a4dd27961f4e41fb24e268436d968c34
jiyoungbb <jiiyoung0906@naver.com>
2022-04-26 21:18:21 +0900

    6.1 useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import Button from "./Button";
 import styles from "./App.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+/* useEffect */
+// 두 개의 argument를 가지는 함수
+// 첫 번째 argument: 한 번만 실행하고 싶은 코드
+// 두 번째 argument: 배열을 넣어줌(다음 강의에)
 
 function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("render");
+  console.log("i run all the time");
+
+  useEffect(() => {
+    console.log("call the api");
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
