dd87aa3fc9ac1fe3e5016391a76c4417697baa5e
jiyoungbb <jiiyoung0906@naver.com>
2022-04-27 20:26:05 +0900

    7.3 movie app part1

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import CoinTracker from './CoinTracker';
+import Movie from './Movie';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <CoinTracker />
+    <Movie />
 );
