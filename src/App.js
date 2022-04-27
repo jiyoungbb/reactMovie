9c78a0d9fb1d8da60764250a3625d211c5d14fa3
jiyoungbb <jiiyoung0906@naver.com>
2022-04-27 12:24:09 +0900

    7.0 todo list part1

--- src/App.js
+++ src/App.js
@@ -2,52 +2,32 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-/* useEffect */
-// 두 개의 argument를 가지는 함수
-// 첫 번째 argument: 한 번만 실행하고 싶은 코드
-// 두 번째 argument:
-//  - []공백일경우 -> 최초 1회 실행
-//  -[keyword,...]값이 있는 경우 -> 해당 값이 변할 때마다 실행
-//  - 여러 개 입력 가능
-
-function Hello(){
-  useEffect(() => {
-    console.log("created:)");
-    return () => console.log("destroyed :(");
-  }, []);
-  return <h1>Hello</h1>;
-}
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("i run all the time");
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo === ""){
+      return;
+    }
+    setToDos(currentArray => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  console.log(toDos);
 
-  useEffect(() => {
-    console.log("i run only once");
-  }, []);
-  useEffect(() => {
-    console.log("i run when 'keyword' change" );
-  }, [keyword]);
-  useEffect(() => {
-    console.log("i run when 'counter' change" );
-  }, [counter]);
-  useEffect(() => {
-    console.log("i run when 'counter' & 'keyword' change" );
-  }, [counter, keyword]);
-
-  const [showing, setShowing] = useState(false);
-  const onClickShow = () => setShowing((prev) => !prev);
-  
   return (
     <div>
-      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
-      <button onClick={onClickShow}>{showing ? "Hide" : "Show"}</button>
-      {showing ? <Hello/>:null}
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          onChange={onChange}
+          value={toDo} 
+          type="text" 
+          placeholder="Wrote your to do..."/>
+          <button>Add To Do</button>
+      </form>
     </div>
   );
 }
